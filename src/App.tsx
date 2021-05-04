92edd779a279869050bb90daeba67294613dc9e9
Seonghui <stellashyou@gmail.com>
2021-05-04 11:52:40 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='App'>
+      <div>
+        <img src='https://picsum.photos/700/300' alt='test' />
+        <img src='https://picsum.photos/700/300' alt='test' />
+        <img src='https://picsum.photos/700/300' alt='test' />
+        <img src='https://picsum.photos/700/300' alt='test' />
+      </div>
+      <div>
+        <button>결제모듈 띄우기</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
